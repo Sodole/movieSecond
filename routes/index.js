7ab64a721f22fad5b99ad71f21338c8fc4234346
Sodole <111020006+Sodole@users.noreply.github.com>
2023-03-28 15:43:14 +0900

    [fix] code번호추가

--- routes/index.js
+++ routes/index.js
@@ -31,17 +31,21 @@ router.get('/filter/:data', async function(req, res, next) {
   let new_data = await JSON.parse(datas)
 
   let new_data2 = await new_data["data"]
-  
+  console.log(new_data2)
   const result = []
+  const code = []
   for (let i = 0; i < new_data2.length; i++) {
       let data = new_data2[i]["title"]
+      let codes = new_data2[i]["code"]
       result.push(data)
+      code.push(codes)
       }
 
   const returns = []
   for (let i = 0; i < result.length; i++) {
       let data = await imageSet(result[i])
-      returns.push(data)
+      let codes = code[i]
+      returns.push({code: codes, data: data})
       }
 
   const result_real = {data : returns }
