20be150fd46db348c692889acc2a2e88475b5f69
Sodole <111020006+Sodole@users.noreply.github.com>
2023-03-28 17:09:00 +0900

    [fix] error처리

--- routes/index.js
+++ routes/index.js
@@ -26,32 +26,35 @@ const imageSet = async(name) => {
 /* GET home page. */
 router.get('/filter/:data', async function(req, res, next) {
   const {data}= req.params
-  const jinsungData = await fetch(`http://3.38.246.123/filter/${data}`)
-  let datas = await jinsungData.json()
-  let new_data = await JSON.parse(datas)
-
-  let new_data2 = await new_data["data"]
-  console.log(new_data2)
-  const result = []
-  const code = []
-  for (let i = 0; i < new_data2.length; i++) {
-      let data = new_data2[i]["title"]
-      let codes = new_data2[i]["code"]
-      result.push(data)
-      code.push(codes)
-      }
-
-  const returns = []
-  for (let i = 0; i < result.length; i++) {
-      let data = await imageSet(result[i])
-      let codes = code[i]
-      returns.push({code: codes, data: data})
-      }
-
-  const result_real = {data : returns }
-
-  res.send(result_real)
+  try{
+   const jinsungData = await fetch(`http://3.38.246.123/filter/${data}`)
+   let datas = await jinsungData.json()
+   let new_data = await JSON.parse(datas)
+ 
+   let new_data2 = await new_data["data"]
+   console.log(new_data2)
+   const result = []
+   const code = []
+   for (let i = 0; i < new_data2.length; i++) {
+       let data = new_data2[i]["title"]
+       let codes = new_data2[i]["code"]
+       result.push(data)
+       code.push(codes)
+       }
+    const returns = []
+    for (let i = 0; i < result.length; i++) {
+        let data = await imageSet(result[i])
+        let codes = code[i]
+        returns.push({code: codes, data: data})
+        }
   
+    const result_real = {data : returns }
+  
+    res.send(result_real)
+  }
+  catch{
+    res.send("진성님 api오류입니다.")
+  }
 });
 
 module.exports = router;
